37927be1b8af43c36a777c18de4c0de3f5e72e85
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-22 18:16:32 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  consolidated_cartconsolidate_cart(cart)
+  consolidated_cart = consolidate_cart(cart)
   apply_coupons(cart, coupons)
   apply_clearance(cart)
   cart_total = []
